789d642dc557d1710f530616fbe0be194ff58490
Andy Awidarto <andy.awidarto@gmail.com>
2013-03-05 01:57:23 +0700

    fix some events

---
+++ application/config/import.php
@@ -0,0 +1,48 @@
+<?php
+	return array(
+
+		'tender_valid_head_selects'=>array(
+
+			'unmapped'=>'unmapped',
+			'no'=>'no',
+            'date'=>'date',
+            'tender_no'=>'tender_no',
+            'clients_tender_no'=>'clients_tender_no',
+            'clients_name' => 	'clients_name',
+            'brief_scope_description' => 	'brief_scope_description',
+            'delivery_terms' => 	'delivery_terms',
+            'closing_date' => 	'closing_date',
+            'tender_system' => 	'tender_system',
+            'bid_price_usd' => 	'bid_price_usd',
+            'bid_price_euro' => 'bid_price_euro',
+            'bid_price_idr' => 'bid_price_idr',
+            'equivalent_bid_price_usd' => 'equivalent_bid_price_usd',
+            'proposed_vendor' => 'proposed_vendor',
+            'person_in_charge' => 'person_in_charge',
+            'status' => 'status',
+            'remarks' => 'remarks'
+		),
+
+		'project_valid_head_selects'=>array(
+
+			'unmapped'=>'unmapped',
+			'no'=>'no',
+            'job_no' => 'job_no',
+            'clients_po_contract_no' => 'clients_po_contract_no',
+            'clients_name' => 	'clients_name',
+            'brief_contract_scope_description' => 'brief_contract_scope_description',
+            'delivery_terms' => 	'delivery_terms',
+            'effective_date' => 'effective_date',
+            'due_date' => 'due_date',
+            'contract_price_usd' => 'contract_price_usd',
+            'contract_price_euro' => 'contract_price_euro',
+            'contract_price_idr' => 'contract_price_idr',
+            'equivalent_contract_price_usd' => 'equivalent_contract_price_usd',
+            'person_in_charge' => 'person_in_charge',
+            'status' => 'status',
+            'remarks' => 'remarks'
+		),
+
+	);
+
+?>
