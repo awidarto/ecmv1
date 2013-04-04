a2ea6c3b09c8cdd20b36ef50f7da6365bc348d0b
Taufiq Ridha <taufiq.ridha@gmail.com>
2013-04-04 10:12:12 +0700

    pull first before dashboard

---
+++ application/views/testcountproject.blade.php
@@ -0,0 +1,9 @@
+@layout('master')
+
+@section('content')
+	
+	<h3>Project per 2013 <?php echo($dataresultvalue) ?></h3>
+	
+	
+
+@endsection
--- application/views/testgraph.blade.php
+++ application/views/testgraph.blade.php
@@ -1,7 +1,16 @@
 @layout('master')
 
 @section('content')
+	
+	<h3>Project per 2013</h3>
+	
+	<img src="{{ URL::to('chart/projecttimeline')}}" alt="graphtest" style="position:left;margin-right:20px;"/>
+	<img src="{{ URL::to('chart/projectpie')}}" alt="graphtest" style="position:left;margin-right:20px;"/>
 
-	<img src="{{ URL::to('graph')}}" alt="graphtest" />
+	<div class="clear">&nbsp;</div>
+	<h3>Tender per 2013</h3>
+	
+	<img src="{{ URL::to('chart/tendertimeline')}}" alt="graphtest" style="position:left;margin-right:20px;"/>
+	<img src="{{ URL::to('chart/projectpie')}}" alt="graphtest" style="position:left;margin-right:20px;"/>
 
 @endsection
