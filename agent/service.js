d57d55cab087f270c1534bb8962e734dee1682bf
Andy Awidarto <andy.awidarto@gmail.com>
2013-09-05 14:32:47 +0700

    move logs outside agent path

--- agent/service.js
+++ agent/service.js
@@ -6,9 +6,9 @@ var forever = require('forever'),
         'watchDirectory': '.',      // Top-level directory to watch from.
         'watchIgnoreDotFiles': true, // whether to ignore dot files
         'watchIgnorePatterns': [], // array of glob patterns to ignore, merged with contents of watchDirectory + '/.foreverignore' file
-        'logFile': 'logs/forever.log', // Path to log output from forever process (when daemonized)
-        'outFile': 'logs/forever.out', // Path to log output from child stdout
-        'errFile': 'logs/forever.err'
+        'logFile': '../logs/forever.log', // Path to log output from forever process (when daemonized)
+        'outFile': '../logs/forever.out', // Path to log output from child stdout
+        'errFile': '../logs/forever.err'
     });
 child.start();
 forever.startServer(child);
