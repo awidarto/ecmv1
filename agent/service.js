49edbe10a805b1b3487047520104c17467d1c43d
Andy Awidarto <andy.awidarto@gmail.com>
2013-09-05 14:37:24 +0700

    move logs outside agent path

--- agent/service.js
+++ agent/service.js
@@ -2,7 +2,7 @@ var forever = require('forever'),
     child = new(forever.Monitor)('maillisten.js', {
         'silent': false,
         'pidFile': 'pids/app.pid',
-        'watch': true,
+        'watch': false,
         'watchDirectory': '.',      // Top-level directory to watch from.
         'watchIgnoreDotFiles': true, // whether to ignore dot files
         'watchIgnorePatterns': [], // array of glob patterns to ignore, merged with contents of watchDirectory + '/.foreverignore' file
