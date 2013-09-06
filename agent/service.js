9ea3b7070715fcb139e65164f085650fbe2f75d8
Andy Awidarto <andy.awidarto@gmail.com>
2013-09-06 12:22:45 +0700

    forever watch

--- agent/service.js
+++ agent/service.js
@@ -1,8 +1,8 @@
 var forever = require('forever'),
     child = new(forever.Monitor)('maillisten.js', {
         'silent': false,
-        'pidFile': 'pids/app.pid',
-        'watch': false,
+        'pidFile': '../pids/app.pid',
+        'watch': true,
         'watchDirectory': '.',      // Top-level directory to watch from.
         'watchIgnoreDotFiles': true, // whether to ignore dot files
         'watchIgnorePatterns': [], // array of glob patterns to ignore, merged with contents of watchDirectory + '/.foreverignore' file
